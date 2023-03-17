83e7de9274a1221e6f4eb377353b583153b80e88
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2023-03-17 13:13:13 +0100

    Update zad_7.py

--- 17_03_2023/zad_7.py
+++ 17_03_2023/zad_7.py
@@ -3,13 +3,15 @@ import numpy as np
 
 def funkcja(n):
     main_result = np.diag([2]*n)
-    heleper = 2
+    helper = 2
     for x in range(n):
-        for y in range(n):
-            if (not main_result[x][y] == 2) and main_result[x][y] < 2*n :
+        main_result[x][0] = helper
+        for y in range(1, n):
+            if (not main_result[x][y] == 2) and main_result[x][y-1] < 2*n :
                 main_result[x][y] = main_result[x][y-1] + 2
-            elif (not main_result[x][y] == 2) and main_result[x][y] == 2*n :
+            elif (not main_result[x][y] == 2) and main_result[x][y-1] >= 2*n :
                 main_result[x][y] = main_result[x][y - 1] - 2
+        helper= helper + 2
     print(main_result)
 
 
