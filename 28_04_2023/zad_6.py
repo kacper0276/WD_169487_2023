87ff949a7628c4cea4cc376411a62b1a342e65dd
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2023-04-28 21:59:04 +0200

    Update zad_6.py

--- 28_04_2023/zad_6.py
+++ 28_04_2023/zad_6.py
@@ -6,16 +6,26 @@ import openpyxl
 data = pd.read_excel("data/imiona.xlsx")
 groupdataM = data[data.Plec == "M"].groupby(by="Rok", as_index=False).agg({"Liczba": "sum"})
 groupdataK = data[data.Plec == "K"].groupby(by="Rok", as_index=False).agg({"Liczba": "sum"})
+groupdata = data.groupby(by="Rok", as_index=False).agg({"Liczba": "sum"})
 
-print(groupdataM["Liczba"])
+arrM = groupdataM["Liczba"].values
+arrK = groupdataK["Liczba"].values
+arr = groupdata["Liczba"].values
+year = groupdataM["Rok"].values
 
+# Wykres z słupkowego 1 podpunkt
+plt.bar(year - 0.2, arrK, color="b", width=0.25, label="Kobiety urodzone w każdym roku")
+plt.bar(year + 0.2, arrM, color="r", width=0.25, label="Mężczyźni urodzeni w każdym roku")
 
+# Wykres liniowy 2 podpunkt
+plt.plot(year, arrK, color="#000", label="Podpunkt 2 Kobiety")
+plt.plot(year, arrM, color="y", label="Podpunkt 2 Mężczyźni")
 
-etykiety = [groupdataM]
-wartosci = [groupdataK]
-c = ["#000000", "#104390"]
-plt.bar(etykiety, wartosci)
-# plt.xticks(rotation=45)
+# Wykres liniowy 3 podpunkt
+plt.bar(year + 0.4, arr, color="#260291", width=0.25, label="Podpunkt 3")
+
+plt.legend()
+plt.xticks(year)
 plt.ylabel('Ilość narodzin')
 plt.xlabel('Rok')
 plt.show()
