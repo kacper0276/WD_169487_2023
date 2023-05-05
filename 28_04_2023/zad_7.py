9aa27b69dc89bfb5a4b921ab5d1c1fcc43162062
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2023-05-05 11:25:00 +0200

    Update zad_7.py

--- 28_04_2023/zad_7.py
+++ 28_04_2023/zad_7.py
@@ -15,7 +15,7 @@ def rzut(n):
 
 
 ilosc = rzut(6)
-rzuty = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+rzuty = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 print(rzuty)
 print(ilosc)
 plt.hist(ilosc, rzuty, facecolor="g")
