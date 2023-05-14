e009fb315700f795eee1ad0f19a3c648249bfb8e
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2023-05-14 09:31:50 +0200

    Update zad_3.py

--- 12_05_2023/zad_3.py
+++ 12_05_2023/zad_3.py
@@ -1,23 +1,25 @@
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
 import openpyxl
-import seaborn as sb
+import seaborn as sns
 
 df = pd.read_excel('./data/imiona.xlsx')
-years = [2010, 2011, 2012, 2013, 2014, 2015]
 
 dfk = df[(df['Plec'] == 'K') & (df["Rok"] >= 2010) & (df["Rok"] <= 2015)]
 dfm = df[(df['Plec'] == 'M') & (df["Rok"] >= 2010) & (df["Rok"] <= 2015)]
 
-dfm = dfm.groupby(by="Rok").agg({"Liczba": "sum"}).reset_index()
-dfk = dfk.groupby(by="Rok").agg({"Liczba": "sum"}).reset_index()
+dfm = dfm.agg({"Liczba": "sum"}).reset_index()
+dfk = dfk.agg({"Liczba": "sum"}).reset_index()
 
-print(dfm.groupby(by="Rok").agg({"Liczba": "sum"}))
-print(dfk.groupby(by="Rok").agg({"Liczba": "sum"}))
+wykres = {"K": dfk[0][0], "M": dfm[0][0]}
 
+print(wykres)
 
-plt.bar(years, dfm["Liczba"], width=0.5)
-plt.bar(years, dfk["Liczba"], width=0.5)
+plt.bar("M",dfm[0][0], width=0.5)
+plt.bar("K",dfk[0][0], width=0.5)
+plt.title("Matplotlib wykres")
 
 plt.show()
+
+sns.barplot(data=wykres, x='Plec', y="Liczba urodzen")
